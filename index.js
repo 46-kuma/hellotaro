ca394af118f0d4c7ae0250f1ccdfc8dffa7429d2
shirokuma <shirokuma240@gmail.com>
2021-08-19 11:55:01 +0900

    heroku db

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express')
 var mysql = require('mysql')
 const app = express()
 
+//ここを追加=============================
+const connection =
+   mysql.createConnection({
+      host:'us-cdbr-east-04.cleardb.com',
+      user:'b3973b93584d55',
+      password:'32dbc54e',
+      database:'heroku_a1c5590b8bde516'
+   });
+//============================================
 
 var port = process.env.PORT || 5000;
 
 app.get('/',(req, res)=>{
-    
-      res.send('helloえくすぷれす');
+//ここを追加===================================================
+connection.query('SELECT * FROM test',(error,results)=>{
+    res.send('hello'+results[0].name+':'+results[0].text);
+      console.log(results)
+      console.log(results[0])
+      console.log(results[0].name)
+      console.log(results[0].text)
+  })
+//===============================================
   })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
